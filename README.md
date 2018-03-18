d2cf541466c455493ca0ce0f08412e6786f51d5e
押野泰平 <toshino@oshinotaiheinoMacBook-Pro.local>
2018-03-18 20:48:16 +0900

    ToDoリスト作成

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # material-ui-exercise
 React + Material-UIサンプル
+## 実装したもの
+* ToDoリスト
+
+# プロジェクト初期構築時コマンド
+```
+$ create-react-app .
+$ npm i --save material-ui@next
+$ npm i --save material-ui-icons
+```
+
+# ビルド
+```
+$ npm run build
+```
+
+# アプリケーション起動
+```
+$ npm run start
+```
---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+
+import AppBar from 'material-ui/AppBar';
+import { withStyles } from 'material-ui/styles';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import Menu, { MenuItem } from 'material-ui/Menu';
+import './App.css';
+import './ToDoList'
+import ToDoList from './ToDoList';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      anchorEl: null,
+    };
+  }
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton onClick={this.handleMenu} className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            Title
+          </Typography>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={open}
+            onClose={this.handleClose}
+          >
+            <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+            <MenuItem onClick={this.handleClose}>My account</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+      <ToDoList />
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
